7d810d6f5ad77328e46aa3ac5a26b42c96fdf81f
cyanray <zyanglin@outlook.com>
2024-02-22 21:12:32 +0800

    Update 02_ParallelForLoop;

--- 02_ParallelForLoop/main.cpp
+++ 02_ParallelForLoop/main.cpp
@@ -5,20 +5,56 @@
 
 using namespace std;
 
+constexpr int NUM_THREADS{14};
+constexpr int N{10000000};
+constexpr double dx = 1.0 / N;
 
 int main()
 {
-    double sum{0};
-#   pragma omp parallel for num_threads(8)
-    for (int i = 1; i <= 10000; ++i)
+    omp_set_num_threads(NUM_THREADS);
+
+    cout << std::format("PI = {}\n", 3.1415926535897932384);
+
+    // 1. use critical
     {
-        double tmp = 1.0 / (i * i);
-#       pragma omp atomic
-        // or # pragma omp critical
-        sum += tmp;
+        double sum{0};
+#       pragma omp parallel for
+        for (int i = 0; i <= N; ++i)
+        {
+            double tmp = 4.0 / (1 + (i * dx) * (i * dx));
+#           pragma omp critical
+            sum += tmp;
+        }
+        sum *= dx;
+        cout << std::format("pi: {}\n", sum);
     }
-    cout << std::format("sum: {}", sum);
-    // sum = pi^2/6 = 1.64493
+
+    // 2. use atomic
+    {
+        double sum{0};
+#       pragma omp parallel for
+        for (int i = 1; i <= N; ++i)
+        {
+            double tmp = 4.0 / (1 + (i * dx) * (i * dx));
+#           pragma omp atomic
+            sum += tmp;
+        }
+        sum *= dx;
+        cout << std::format("pi: {}\n", sum);
+    }
+
+    // 3. use reduction
+    {
+        double sum{0};
+#       pragma omp parallel for reduction(+:sum)
+        for (int i = 1; i <= N; ++i)
+        {
+            double tmp = 4.0 / (1 + (i * dx) * (i * dx));
+            sum += tmp;
+        }
+        sum *= dx;
+        cout << std::format("pi: {}\n", sum);
+    }
 
     return 0;
 }
